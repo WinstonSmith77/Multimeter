0d00c053671b1c4d9cfac7e168f86ae8eb614abf
Matthias Henning <Matze_GitHub@gmx.de>
2016-10-29 19:08:06 +0200

    Refactoring

---
+++ Deocder/AtomicTypes.fs
@@ -0,0 +1,15 @@
+﻿module AtomicTypes
+
+[<System.FlagsAttribute>]
+     type LowerBits =
+           |    One = 1
+           |    Two = 2
+           |    Three = 4
+           |    Four = 8
+           |    All = 15
+           |    Five = 16
+  
+    type DecodedBuffer =   
+        {
+            Buffer:byte array
+        }   
--- Deocder/VC_840Decoder.fs
+++ Deocder/VC_840Decoder.fs
@@ -1,20 +1,7 @@
 ﻿module VC_840Decoder
-    
-    let numberOfBytesInTelegram = 14
+	open AtomicTypes
 
-    [<System.FlagsAttribute>]
-     type LowerBits =
-           |    One = 1
-           |    Two = 2
-           |    Three = 4
-           |    Four = 8
-           |    All = 15
-           |    Five = 16
-  
-    type DecodedBuffer =   
-        {
-            Buffer:byte array
-        }   
+    let numberOfBytesInTelegram = 14
 
     let getIndex rawByte = rawByte / byte(LowerBits.Five)
     let getData rawByte = rawByte &&& byte(LowerBits.All)
