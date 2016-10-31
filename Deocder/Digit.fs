d63675e23b806224a5f1cb611c58b8b9ca436ad3
mhenning <mhenning@tech2go.de>
2016-10-31 19:03:42 +0100

    Begin digits

---
+++ Deocder/Digit.fs
@@ -0,0 +1,17 @@
+﻿module Digit
+    type SevenSegment=
+        | TopCenter
+        | Center
+        | BottomCenter
+        | TopLeft
+        | Topright
+        | BottomLeft
+        | BottomRight  
+
+    type Digit = {
+        Segments : SevenSegment Set
+        }
+
+    let One = { Segments = [SevenSegment.BottomRight; SevenSegment.Topright] |> Set.ofList }
+
+   
