c570a4b32b43dd37d238b461e790fcb92211b178
mhenning <mhenning@tech2go.de>
2016-12-15 17:46:57 +0100

    More Tests

--- Deocder/BufferHandling.fs
+++ Deocder/BufferHandling.fs
@@ -3,7 +3,7 @@ module BufferHandling
     open Data
     open TelegramData
 
-    let numberOfBytesInTelegram = 14
+    let NumberOfBytesInTelegram = 14
 
     let IsNotStartByte value =
          value / byte(Bits.Five) <> byte(1)   
@@ -13,21 +13,22 @@ module BufferHandling
         | [] -> None    
         | _  -> match IsNotStartByte (List.head buffer) with    
                 | false -> match List.length buffer with
-                           | length when length >= numberOfBytesInTelegram ->  Some(List.take numberOfBytesInTelegram buffer)
+                           | length when length >= NumberOfBytesInTelegram ->  Some(List.take NumberOfBytesInTelegram buffer)
                            | _ -> None
                 | true  -> FindValidSequence  (List.tail buffer)
 
     let MergeBuffers a b =
-        List.concat [List.ofSeq a; List.ofSeq b]   
+        Array.concat [Array.ofSeq a; Array.ofSeq b]   
 
-    let GetAllValidBlocksFromBuffer buffer =
+    let GetAllValidBlocksFromBuffer bufferInput =
+       let buffer = List.ofSeq bufferInput 
        let rec getAllValidBlockFromBufferInner dataAndBuffer =
           let (dataList, buffer) = dataAndBuffer
          
           let parseFrom = FindValidSequence buffer
 
           match(parseFrom) with
-          | Some(x) -> getAllValidBlockFromBufferInner (x :: dataList,  List.skipWhile IsNotStartByte  buffer |> List.skip numberOfBytesInTelegram )
+          | Some(x) -> getAllValidBlockFromBufferInner (x :: dataList,  List.skipWhile IsNotStartByte  buffer |> List.skip NumberOfBytesInTelegram )
           | None    -> dataAndBuffer
 
        getAllValidBlockFromBufferInner ([], buffer)  
---
+++ Tests/BufferHandlingTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    using FluentAssertions;
+
+    using Microsoft.FSharp.Collections;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public static class BufferHandlingTest
+    {
+        [Test]
+        public static void SimpleTest()
+        {
+            var data = CreateRandomData();
+            var buffer = data.Select((dataItem, index) => (byte)(dataItem + (index + 1) * 16)).ToArray();
+
+            var output = BufferHandling.GetAllValidBlocksFromBuffer(buffer).Item1.First().ToArray();
+
+            output.Should().BeEquivalentTo(buffer);
+        }
+
+        private static byte[] CreateRandomData()
+        {
+            var random = new Random();
+
+            return Enumerable.Range(0, BufferHandling.NumberOfBytesInTelegram).Select(index => (byte)random.Next(16)).ToArray();
+        }
+    }
+}
